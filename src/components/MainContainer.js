896378580743937dfead9e1ed06c8d32841c2d6c
Doris Morales <moralesdoris880@gmail.com>
2021-12-30 16:05:54 -0500

    First Deliverable Done and working on second

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const[stocks,setStocks]= useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:3001/stocks")
+    .then(response => response.json())
+    .then(data => setStocks(data))
+  },[]);
+
+  function handleAddStock(e){
+    return console.log(e.target)
+  }
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} onAddStock={handleAddStock}/>
         </div>
         <div className="col-4">
           <PortfolioContainer />
